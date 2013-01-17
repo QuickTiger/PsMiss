33347dd222ef04c60ad57ca807d7790a8d2c9507
QuickTiger <wjzhh815@gmail.com>
2013-01-17 17:33:32 +0800

    A scripts of download the Data  of the lake monitor from TaiHu

---
+++ GetDataOfLake.py
@@ -0,0 +1,17 @@
+import re
+import urllib.request
+f=open('data.txt','w')
+for i in range(60617-54291):
+    n=i+54291
+    print(n)
+    data_url="http://www.lakesci.csdb.cn/page/showItem.vpage?id=lakesci.csdb.cn.rgjc.zdhpszzdjc/"+str(n)
+    s=urllib.request.urlopen(data_url)
+    data=s.read().decode('utf-8')
+    pattern=re.compile(r'(?<=6px;\" >﻿)[\d.]+')
+    #print(data)
+    #print(pattern.search(data).group())
+    mm=pattern.findall(data)
+    for item in mm:
+        f.write(item+'\t')
+    f.write('\n')
+f.close()
